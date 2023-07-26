105e488f5a303e0dc27d1bdbae204977221adc9d
prajwalbandak <62823252+prajwalbandak@users.noreply.github.com>
2023-07-26 22:17:13 +0530

    Update InputField.tsx

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,11 @@
 import React, { useRef } from 'react'
 import "./styles.css"
 
-
 interface Props{
     todo:string;
     setTodo:React.Dispatch<React.SetStateAction<string>>;
     handleAdd : (e: React.FormEvent) => void;
- 
 }
-
 const InputField = ({todo, setTodo, handleAdd } : Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
   return (
@@ -19,9 +16,7 @@ const InputField = ({todo, setTodo, handleAdd } : Props) => {
     }} > 
         <input type="input" placeholder="enter a task" className="input__box" value={todo} ref={inputRef} onChange={(e) => setTodo(e.target.value)}></input>
         <button className='input_submit' type='submit'>GO</button>
-        
-        
-         </form>
+     </form>
   )
 }
 
